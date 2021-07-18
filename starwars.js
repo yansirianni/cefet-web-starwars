cdc1466c8a9e506c5c5893214c50c75f611e1853
Yan Victor <yanvictorsa@yahoo.com.br>
2021-07-18 16:10:21 -0300

    Add music player - Exercice 1

--- starwars.js
+++ starwars.js
@@ -4,4 +4,15 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
+import { play } from './music.js';
+
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+const musicConfiguration = {
+    audioUrl: 'audio/tema-sw.mp3',
+    coverImageUrl: 'imgs/logo.svg',
+    title: 'Intro',
+    artist: 'John Williams'
+};
+
+play(musicConfiguration, document.body);
