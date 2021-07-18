1bc322e96b6c0bc64df215dd181489822f94e5d9
Yan Victor <yanvictorsa@yahoo.com.br>
2021-07-18 18:25:23 -0300

    Fill movies list - Exercice 2

---
+++ roman.js
@@ -0,0 +1,12 @@
+export function numeroParaRomano(numero) {
+    const dados = {
+       '1': 'I',
+       '2': 'II',
+       '3': 'III',
+       '4': 'IV',
+       '5': 'V',
+       '6': 'VI'
+    }
+
+    return dados[numero]
+ }
--- starwars.js
+++ starwars.js
@@ -5,8 +5,11 @@
 //  - Quando um filme for clicado, exibir sua introdução
 
 import { play } from './music.js';
+import { numeroParaRomano } from './roman.js';
 
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+//Exercicio 1
 
 const musicConfiguration = {
     audioUrl: 'audio/tema-sw.mp3',
@@ -16,3 +19,16 @@ const musicConfiguration = {
 };
 
 play(musicConfiguration, document.body);
+
+//Exercicio 2
+
+const response = await fetch(`${API_ENDPOINT}/films`);
+const films = await response.json();
+
+const filmsListEl = document.querySelector('#filmes ul');
+
+filmsListEl.innerHTML = '';
+
+for (const film of films.results){
+    filmsListEl.innerHTML += `<li>Episode ${numeroParaRomano(film.episode_id).padEnd(3, ' ')} - ${film.title}</li>`;
+}
